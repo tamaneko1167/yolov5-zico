e674261c02a10a6550e7a18bb9b3d801ae8b7838
tamaneko1167 <yuritamaneko1167@gmail.com>
2025-08-20 17:50:54 +0200

    add w020yaml file

--- map_latency_pareto.py
+++ map_latency_pareto.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 data = [
     (0.33, True,  1.0, 0.4830, 1.75, 572.48),  # baseline
     (0.33, False,  1.5, 0.3988, 2.05, 488.26),  # best
-    (0.33, True, 1.5, 0.4842, 1.61, 621.28),
+    (0.33, True, 1.5, 0.4776, 1.80, 555.18),
     (0.76, False,  1.0, 0.4874, 1.60, 624.42),
 ]
 
@@ -19,7 +19,7 @@ lat_ms  = np.array([d[5] for d in data])  # ms
 
 # ====== baseline と best を指定 ======
 baseline_idx = 0
-best_idx = 1
+#best_idx = 1
 
 def plot_graph(x, x_label, title):
     plt.figure(figsize=(8,6))
@@ -31,7 +31,7 @@ def plot_graph(x, x_label, title):
     plt.scatter(x[baseline_idx], mAP[baseline_idx], color="red", s=100, label="Baseline")
 
     # best を緑で強調
-    plt.scatter(x[best_idx], mAP[best_idx], color="green", s=100, label="Best")
+    #plt.scatter(x[best_idx], mAP[best_idx], color="green", s=100, label="Best")
 
     # ====== 全部にラベル表示 ======
     for i in range(len(data)):
@@ -42,9 +42,9 @@ def plot_graph(x, x_label, title):
     # baseline と best には追加で注釈
     plt.annotate("Baseline", (x[baseline_idx], mAP[baseline_idx]),
                  fontsize=9, xytext=(10,-10), textcoords="offset points", color="red")
-    improve = (mAP[best_idx]-mAP[baseline_idx])/mAP[baseline_idx]*100
-    plt.annotate(f"Best (+{improve:.1f}%)", (x[best_idx], mAP[best_idx]),
-                 fontsize=9, xytext=(10,10), textcoords="offset points", color="green")
+    # improve = (mAP[best_idx]-mAP[baseline_idx])/mAP[baseline_idx]*100
+    # plt.annotate(f"Best (+{improve:.1f}%)", (x[best_idx], mAP[best_idx]),
+    #              fontsize=9, xytext=(10,10), textcoords="offset points", color="green")
 
     plt.xlabel(x_label)
     plt.ylabel("mAP ↑ bigger is better")
